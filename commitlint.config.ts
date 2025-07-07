7403113bd44152dc10d45e6a72b09d4b251a6932
vatsal <vatsalsinghkv@gmail.com>
2025-07-07 16:58:57 +0530

    feat: commitlint add

---
+++ commitlint.config.ts
@@ -0,0 +1,26 @@
+import type { UserConfig } from '@commitlint/types';
+
+const Configuration: UserConfig = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [
+      2,
+      'always',
+      [
+        'feat',
+        'fix',
+        'docs',
+        'chore',
+        'style',
+        'refactor',
+        'ci',
+        'test',
+        'perf',
+        'revert',
+        'vercel',
+      ],
+    ],
+  },
+};
+
+export default Configuration;
